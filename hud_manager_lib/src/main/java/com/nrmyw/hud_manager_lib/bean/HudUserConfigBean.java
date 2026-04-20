dd98c7ef3f8e6e59b98c3be6423804c93277fad2
Li Xiao <lixiao@nb-666.com>
2026-04-20 21:16:41 +0800

    更新代码关系

---
+++ hud_manager_lib/src/main/java/com/nrmyw/hud_manager_lib/bean/HudUserConfigBean.java
@@ -0,0 +1,9 @@
+package com.nrmyw.hud_manager_lib.bean;
+
+import java.io.Serializable;
+
+public class HudUserConfigBean implements Serializable {
+
+
+
+}
---
+++ hud_manager_lib/src/main/java/com/nrmyw/hud_manager_lib/config/HudUserSetConfig.java
@@ -0,0 +1,36 @@
+package com.nrmyw.hud_manager_lib.config;
+
+import com.nrmyw.hud_manager_lib.bean.HudUserConfigBean;
+
+public class HudUserSetConfig {
+    private static HudUserSetConfig hudUserSetConfig;
+    private HudUserConfigBean userConfigBean;
+    private HudUserSetConfig(){
+
+    }
+
+    public static HudUserSetConfig getInstance(){
+        if(null==hudUserSetConfig){
+            synchronized (HudUserSetConfig.class){
+                if(null==hudUserSetConfig){
+                    hudUserSetConfig=new HudUserSetConfig();
+                }
+            }
+        }
+        return hudUserSetConfig;
+    }
+
+    public void setUserConfigBean(HudUserConfigBean hudUserConfigBean){
+        this.userConfigBean=hudUserConfigBean;
+    }
+
+
+    public HudUserConfigBean getUserConfigBean(){
+        if(null==userConfigBean){
+            userConfigBean=new HudUserConfigBean();
+        }
+        return userConfigBean;
+    }
+
+
+}
